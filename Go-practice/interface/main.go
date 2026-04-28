98d6ae8349e267182aba6f8e9ab4c4fe9011059d
iokunade <israeloluwasegun293@gmail.com>
2026-04-28 14:55:01 +0100

    understanding interface now

--- Go-practice/interface/main.go
+++ Go-practice/interface/main.go
@@ -2,30 +2,67 @@ package main
 
 import "fmt"
 
-type person struct {
-	firstname string
-	lastname  string
+type Person struct {
+	Firstname string
+	Lastname  string
 }
 
-// type behaviour interface {
-// 	walk()
-// 	talk()
-// }
+type Robot struct {
+	Name  string
+	Model string
+}
 
+type Behavior interface {
+	walk() string
+	talk() string
+	sing() string
+	playKeyboard() bool
+}
+
+func (p *Person) walk() string {
+	return "Walks normal like human being"
+}
+
+func (p *Person) talk() string {
+	return "talks like audio"
+}
+
+func (p *Person) sing() string {
+	return "sings beautifully"
+}
+
+func (p *Person) playKeyboard() bool {
+	return true
+}
+
+func (r *Robot) walk() string {
+	return "rolls on wheel"
+}
+
+func (r *Robot) talk() string {
+	return "talks like audio"
+}
+
+func (r *Robot) sing() string {
+	return "Plays audio files"
+}
+
+func (r *Robot) playKeyboard() bool {
+	return true
+}
+
+
+
+func describe(b Behavior){
+	fmt.Println(b.walk())
+	fmt.Println(b.talk())
+	fmt.Println(b.sing())
+	fmt.Println(b.playKeyboard())
+}
 func main() {
+	p := &Person{Firstname: "IJhayclassics"}
+	r := &Robot{Name: "Jarvis"}
 
-	p := new(person)
-	fmt.Println(p.walk())
-
-	t := new(person)
-	fmt.Println(t.talk())
-}
-
-func (w *person) walk() string {
-	return "Have a good day"
-
-}
-
-func (w *person) talk() string {
-	return "I am a Boy"
+	describe(p)
+	describe(r)
 }
